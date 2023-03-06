68c18ef6388198651d598afebf1afc781340fa1e
Diogo Antunes <diogo.santiago@tecnico.ulisboa.pt>
2023-03-06 13:22:25 +0000

    feat: implemented Account class

--- DistLedgerServer/src/main/java/pt/tecnico/distledger/server/domain/Account.java
+++ DistLedgerServer/src/main/java/pt/tecnico/distledger/server/domain/Account.java
@@ -1,5 +1,54 @@
 package pt.tecnico.distledger.server.domain;
 
+import pt.tecnico.distledger.server.domain.exceptions.NotEnoughBalanceException;
+
 public class Account {
-    
+    private int balance;
+    private String userId;
+
+    private static final int TOTAL_COIN = 1000;
+    private static final Account broker = new Account("__BROKER__", TOTAL_COIN);
+
+    private Account(String userId, int initialBalance) {
+        this.userId = userId;
+        this.balance = initialBalance;
+    }
+
+    public Account(String userId) {
+        this(userId, 0);
+    }
+
+    public static Account getBroker() {
+        return broker;
+    }
+
+    public void increaseBalance(int amount) {
+        this.balance += amount;
+    }
+
+    public void decreaseBalance(int amount) throws NotEnoughBalanceException {
+        if (amount <= balance) {
+
+        }
+        throw new NotEnoughBalanceException(balance, amount);
+    }
+
+    public int getBalance() {
+        return balance;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (obj instanceof Account) {
+            Account a = (Account) obj; 
+            return a.userId.compareTo(userId) == 0;
+        }
+
+        return false;
+    }
+
+    @Override
+    public int hashCode() {
+        return userId.hashCode();
+    }
 }
--- DistLedgerServer/src/main/java/pt/tecnico/distledger/server/domain/exceptions/NotEnoughBalanceException.java
+++ DistLedgerServer/src/main/java/pt/tecnico/distledger/server/domain/exceptions/NotEnoughBalanceException.java
@@ -1,5 +1,19 @@
 package pt.tecnico.distledger.server.domain.exceptions;
 
 public class NotEnoughBalanceException extends DistLedgerException {
-    
+    private int availableBalance;
+    private int desiredAmount;
+
+    public NotEnoughBalanceException(int availableBalance, int desiredAmount) {
+        this.availableBalance = availableBalance;
+        this.desiredAmount = desiredAmount;
+    }
+
+    public int getAvailableBalance() {
+        return availableBalance;
+    }
+
+    public int getDesiredAmount() {
+        return desiredAmount;
+    }
 }
