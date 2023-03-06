f4646578876ec8f500811785a04a99b0839155e9
Francisco Lopes <francisco.a.lopes@tecnico.ulisboa.pt>
2023-03-06 10:58:19 +0000

    Added ServerMain logic

--- DistLedgerServer/src/main/java/pt/tecnico/distledger/server/ServerMain.java
+++ DistLedgerServer/src/main/java/pt/tecnico/distledger/server/ServerMain.java
@@ -1,9 +1,32 @@
 package pt.tecnico.distledger.server;
+
+import io.grpc.Server;
+import io.grpc.ServerBuilder;
+
+import java.io.IOException;
+
+
 public class ServerMain {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException, InterruptedException {
 
-        /* TODO */
+        if (args.length < 1) {
+            System.err.println("Argument(s) missing!");
+            System.err.printf("Usage: java %s server%n", ServerMain.class.getName());
+            return;
+        }
+
+        String sv = args[0];
+        final int port = Integer.parseInt(args[0]);
+
+
+        Server server = ServerBuilder.forPort(port).build();
+
+        server.start();
+
+        System.out.println("Server started, listening on " + port);
+
+        server.awaitTermination();
 
     }
 
