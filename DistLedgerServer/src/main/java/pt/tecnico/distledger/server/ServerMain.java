4c6ce8979c9aad8d205bf73f939cf3d830c7a7a2
Diogo Antunes <diogo.santiago@tecnico.ulisboa.pt>
2023-03-07 19:13:42 +0000

    feat: added ServerMain implementation

--- DistLedgerServer/src/main/java/pt/tecnico/distledger/server/ServerMain.java
+++ DistLedgerServer/src/main/java/pt/tecnico/distledger/server/ServerMain.java
@@ -1,10 +1,35 @@
 package pt.tecnico.distledger.server;
+
+import io.grpc.Server;
+import io.grpc.ServerBuilder;
+import pt.tecnico.distledger.server.grpc.AdminServiceImpl;
+import pt.tecnico.distledger.server.grpc.UserServiceImpl;
+
+import java.io.IOException;
+
+
 public class ServerMain {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException, InterruptedException {
 
-        /* TODO */
+        if (args.length < 2) {
+            System.err.println("Argument(s) missing!");
+            System.err.printf("Usage: java %s port qual", ServerMain.class.getName());
+            return;
+        }
 
+        final int port = Integer.parseInt(args[0]);
+
+        Server server = ServerBuilder.forPort(port)
+                                     .addService(new UserServiceImpl())
+                                     .addService(new AdminServiceImpl())
+                                     .build();
+
+        server.start();
+
+        System.out.println("Server started, listening on " + port);
+
+        server.awaitTermination();
     }
 
 }
