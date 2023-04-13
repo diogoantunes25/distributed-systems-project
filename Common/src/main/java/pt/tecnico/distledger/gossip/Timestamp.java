64d39a47c7c3d64eb60fb4c8e7912ab2a06844a9
João Rocha <joao.t.f.rocha@tecnico.ulisboa.pt>
2023-04-13 13:32:53 +0100

    refactor: clean Timestamp code

--- Common/src/main/java/pt/tecnico/distledger/gossip/Timestamp.java
+++ Common/src/main/java/pt/tecnico/distledger/gossip/Timestamp.java
@@ -1,6 +1,14 @@
 package pt.tecnico.distledger.gossip;
 
-import java.util.*;
+import java.util.Map;
+import java.util.HashMap;
+import java.util.Set;
+import java.util.stream.Collectors;
+import java.util.HashSet;
+import java.util.Collection;
+import java.util.Comparator;
+
+import pt.ulisboa.tecnico.distledger.contract.DistLedgerCommonDefinitions;;
 
 public class Timestamp {
     private Map<String, Integer> times;
@@ -13,10 +21,7 @@ public class Timestamp {
         this.times = times;
     }
 
-    public synchronized void updateTime(String target, int time) {
-        times.put(target, time);
-    }
-
+    // Getters
     public synchronized Map<String, Integer> getTimes() {
         return times;
     }
@@ -30,13 +35,7 @@ public class Timestamp {
         return times.keySet();
     }
 
-    public static boolean lessOrEqual(Timestamp t1, Timestamp t2) {
-        for(String target: t1.getNonNullReplicas())
-            if (t1.getTime(target) > t2.getTime(target)) return false;
-
-        return true;
-    }
-
+    // Setters
     public synchronized Timestamp set(String target, int value) {
         times.put(target, value);
         return this;
@@ -53,27 +52,27 @@ public class Timestamp {
         this.times = newTimes;
     }
 
-    public synchronized Timestamp increaseAndGetCopy(String target) {
-        times.putIfAbsent(target, 0);
-        times.put(target, times.get(target)+1);
-        return getCopy();
-    }
-
+    // Copy
     public synchronized Timestamp getCopy() {
         Map<String, Integer> copy = new HashMap<>();
-        for (String q: times.keySet()) copy.put(q, times.get(q));
+        for (String target: times.keySet()) copy.put(target, this.getTime(target));
 
         return new Timestamp(copy);
     }
 
-    public static Timestamp fromGrpc(pt.ulisboa.tecnico.distledger.contract.DistLedgerCommonDefinitions.Timestamp t) {
+    public synchronized Timestamp increaseAndGetCopy(String target) {
+        return this.set(target, this.getTime(target)+1).getCopy();
+    }
+
+    // Serialization
+    public static Timestamp fromGrpc(DistLedgerCommonDefinitions.Timestamp t) {
         Map<String, Integer> times = t.getTimestampMap();
         return new Timestamp(times);
     }
 
-    public pt.ulisboa.tecnico.distledger.contract.DistLedgerCommonDefinitions.Timestamp toGrpc() {
-        pt.ulisboa.tecnico.distledger.contract.DistLedgerCommonDefinitions.Timestamp.Builder builder =
-                pt.ulisboa.tecnico.distledger.contract.DistLedgerCommonDefinitions.Timestamp.newBuilder();
+    public DistLedgerCommonDefinitions.Timestamp toGrpc() {
+            DistLedgerCommonDefinitions.Timestamp.Builder builder =
+                    DistLedgerCommonDefinitions.Timestamp.newBuilder();
 
         for (String target: times.keySet()) {
             builder.putTimestamp(target, times.get(target));
@@ -82,55 +81,51 @@ public class Timestamp {
         return builder.build();
     }
 
+    // Comparators
+    public static boolean lessOrEqual(Timestamp t1, Timestamp t2) {
+        for(String target: t1.getNonNullReplicas())
+            if (t1.getTime(target) > t2.getTime(target)) return false;
+
+        return true;
+    }
+
     @Override
     public boolean equals(Object o) {
         if (this == o) return true;
         if (o == null || getClass() != o.getClass()) return false;
         Timestamp timestamp = (Timestamp) o;
 
-        for (String target: times.keySet()) {
-            Integer t = timestamp.getTimes().get(target);
-            if (t == null) return false;
-            if (t != times.get(target)) return false;
-        }
-
-        for (String target: timestamp.getNonNullReplicas()) {
-            Integer t = times.get(target);
-            if (t == null) return false;
-            if (t != timestamp.getTime(target)) return false;
-        }
+        for (String target: this.getNonNullReplicas())
+            if (this.getTime(target) != timestamp.getTime(target)) return false;
+        for (String target: timestamp.getNonNullReplicas())
+            if (this.getTime(target) != timestamp.getTime(target)) return false;
 
         return true;
     }
 
+    /*
+     * A total order comparator is necessary to unambiguously order timestamps.
+     * Note that ordering timestamps might be necessary in order to guarantee
+     * that updates received in a gossip are applied in the correct order.
+     */
     public static Comparator<Timestamp> getTotalOrderPrevComparator() {
         return (o1, o2) -> {
             Map<String, Integer> t1 = o1.getTimes();
             Map<String, Integer> t2 = o2.getTimes();
 
             if (o1.equals(o2)) return 0;
-
-            // o1 < o2
-            boolean o1Smaller = true;
-            for (String target: t1.keySet())
-                if (t1.get(target) > t2.get(target)) o1Smaller = false;
-
-            if (o1Smaller) return -1;
-
-            // o2 < o1
-            boolean o2Smaller = true;
-            for (String target: t2.keySet())
-                if (t2.get(target) > t1.get(target)) o2Smaller = false;
-
-            if (o2Smaller) return 1;
+            if (lessOrEqual(o1, o2)) return -1;
+            if (lessOrEqual(o2, o1)) return 1;
 
             // Extra comparison to establish total order
             Set<String> targets = new HashSet<>();
             targets.addAll(t1.keySet());
             targets.addAll(t2.keySet());
-            targets.stream().sorted();
+            targets = targets.stream().sorted().collect(Collectors.toSet());
 
             for (String target: targets) {
+                if (!t1.containsKey(target)) return -1;
+                if (!t2.containsKey(target)) return 1;
                 if (t1.get(target) > t2.get(target)) return 1;
                 if (t2.get(target) > t1.get(target)) return -1;
             }
--- NamingServer/src/main/java/pt/tecnico/distledger/namingserver/NamingServer.java
+++ NamingServer/src/main/java/pt/tecnico/distledger/namingserver/NamingServer.java
@@ -6,7 +6,6 @@ import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
 import java.util.concurrent.atomic.AtomicLong;
-import java.util.stream.Collectors;
 
 import io.grpc.Server;
 import io.grpc.ServerBuilder;
